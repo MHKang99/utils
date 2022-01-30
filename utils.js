be9725f0bee97a79c86d79923da379bb97430c31
Min <minkang99@hotmail.com>
2022-01-29 18:23:28 -0800

    modified utils.js to have divide function.

--- utils.js
+++ utils.js
@@ -2,4 +2,10 @@ function product(num1, num2) {
     console.log(num1 * num2); 
 }
 
-product(process.argv[2], process.argv[3])
+product(process.argv[2], process.argv[3]);
+
+function divide(num1, num2) {
+    console.log(num1/num2); 
+}
+
+divide(process.argv[2], process.argv[3])
